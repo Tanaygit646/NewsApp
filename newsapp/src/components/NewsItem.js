23b049739b492789658fdb44ef2e6699ba3628b3
Tanaygit646 <tanay90nta@gmail.com>
2026-03-03 21:19:41 +0530

    Adding navbar and news.js file

---
+++ newsapp/src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello I am tanay saha. I am from INDIA</p>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
