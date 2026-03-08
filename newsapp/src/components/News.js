1afd5fc59b6a3de98853eacf6b5aa4cd45ea55f9
Tanaygit646 <tanay90nta@gmail.com>
2026-03-08 12:20:42 +0530

    feat: add navbar categories and improve News component with PropTypes and defaultProps

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
-  constructor(){
-    super();
+
+  static defaultProps = {
+    country:'us',
+    pagesize:3,
+    category:'general'
+  };
+
+  static propTypes = {
+    country : PropTypes.string,
+    pagesize : PropTypes.number,
+    category:PropTypes.string
+  };
+
+  constructor(props){
+    super(props);
     console.log("hello");
     this.state={
       articles:[],
@@ -14,17 +28,17 @@ export class News extends Component {
   }
 
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/everything?q=apple&from=2026-03-05&to=2026-03-05&sortBy=popularity&apiKey=a3063431959341b48ca277d919a5e679";
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&category=${this.props.category}&apiKey=a3063431959341b48ca277d919a5e679`;
     this.setState({loading:true});
     let data =await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles:parsedData.articles});
+    this.setState({articles:parsedData.articles,loading:false});
   }
 
   handleNextClick= async()=>{
     if(!(this.state.page+1 > Math.ceil(this.state.totalResults/2))){
-    let url = `https://newsapi.org/v2/everything?q=apple&from=2026-03-05&to=2026-03-05&sortBy=popularity&apiKey=a3063431959341b48ca277d919a5e679&page=${this.state.page+1}&pagesize=5`
+    let url = ` https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a3063431959341b48ca277d919a5e679&page=${this.state.page+1}&pagesize=${this.props.pagesize}`
     this.setState({loading:true});
     let data =await fetch(url);
     let parsedData = await data.json();
@@ -37,7 +51,7 @@ export class News extends Component {
   }
 
   handlePreviousClick= async()=>{
-    let url = `https://newsapi.org/v2/everything?q=apple&from=2026-03-05&to=2026-03-05&sortBy=popularity&apiKey=a3063431959341b48ca277d919a5e679&page=${this.state.page-1}&pagesize=5`
+    let url = ` https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a3063431959341b48ca277d919a5e679&page=${this.state.page-1}&pagesize=${this.props.pagesize}`
     this.setState({loading:true})
     let data =await fetch(url);
     let parsedData = await data.json();
@@ -51,7 +65,7 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1 className="container my-3 text-center">NewsMonkey - Top Headlines</h1>
+        <h1 className="container my-3 text-center" style={{margin:'40px-0px'}}>NewsMonkey - Top Headlines</h1>
         {this.state.loading && <Spinner/>}        
         <div className="row">
           {!this.state.loading && this.state.articles.map((e)=>{
