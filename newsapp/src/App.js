84d1f13466335dac9041d57864c0843e722cfa05
Tanaygit646 <tanay90nta@gmail.com>
2026-03-09 08:57:38 +0530

    fect: reacr-router-dom and link the categories of news in the navbar

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,16 +1,54 @@
-import './App.css';
-import React, { Component } from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-          <Navbar/>
-          <News pagesize={5} country="us" categoty="technology"/>
-      </div>
-    )
+      <Router>
+        <Navbar />
+
+        <Routes>
+
+          <Route
+            path="/"
+            element={<News key="general" pagesize={5} country="us" category="general" />}
+          />
+
+          <Route
+            path="/entertainment"
+            element={<News key="entertainment" pagesize={5} country="us" category="entertainment" />}
+          />
+          
+          <Route
+            path="/business"
+            element={<News key="business" pagesize={5} country="us" category="business" />}
+          />
+
+          <Route
+            path="/health"
+            element={<News key="health" pagesize={5} country="us" category="health" />}
+          />
+
+          <Route
+            path="/science"
+            element={<News key="science" pagesize={5} country="us" category="science" />}
+          />
+
+          <Route
+            path="/sports"
+            element={<News key="sports" pagesize={5} country="us" category="sports" />}
+          />
+
+          <Route
+            path="/technology"
+            element={<News key="technology" pagesize={5} country="us" category="technology" />}
+          />
+
+        </Routes>
+      </Router>
+    );
   }
 }
-
